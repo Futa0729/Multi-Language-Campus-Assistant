04236e08ba57c969695def7b3bb45e6d56146333
futa0729 <f.n.200507209@gmail.com>
2025-12-02 14:11:48 -0700

    Add CampusBuildingUI

---
+++ CampusAssistant/UI/CampusBuildingUI.cs
@@ -0,0 +1,62 @@
+using System;
+using CampusAssistant.Models;
+using CampusAssistant.Services;
+using CampusAssistant.DataLayer;
+
+
+namespace CampusAssistant.UI
+{
+    public class CampusBuildingUI
+    {
+        private readonly LanguageService _languageService;
+        public CampusBuildingUI(LanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+
+        public void ShowCampusBuildings()
+        {
+            List<CampusBuilding> buildings = CampusStore.GetAllBuildings();
+
+            if (_languageService.CurrentLanguage == Language.English)
+            {
+                Console.WriteLine("Campus Buildings:");
+            }
+            else if (_languageService.CurrentLanguage == Language.Japanese)
+            {
+                Console.WriteLine("キャンパスの建物:");
+            }
+
+
+            foreach (CampusBuilding building in buildings)
+            {
+                string name = "";
+                string description = "";
+
+                if (_languageService.CurrentLanguage == Language.English)
+                {
+                    name = building.NameEn;
+                    description = building.DescriptionEn;
+                }
+                else if (_languageService.CurrentLanguage == Language.Japanese)
+                {
+                    name = building.NameJp;
+                    description = building.DescriptionJp;
+                }
+
+                Console.WriteLine($"- {name}: {description}");
+            }
+
+            if (_languageService.CurrentLanguage == Language.English)
+            {
+                Console.WriteLine("Press any key to return to menu.");
+            }
+            else if (_languageService.CurrentLanguage == Language.Japanese)
+            {
+                Console.WriteLine("メニューに戻るには任意のキーを押してください。");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
